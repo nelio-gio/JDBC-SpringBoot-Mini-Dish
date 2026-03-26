3c96de96268d01c2098e19b08a150feddf735722
nelio-gio <hei.rocques@gmail.com>
2026-03-26 23:06:15 +0100

    Initial: JDBC et Spring Boot

---
+++ src/main/java/org/example/minidishapi/MiniDishApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.minidishapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniDishApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniDishApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/minidishapi/MiniDishApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.minidishapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniDishApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
